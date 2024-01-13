ddc474d2e79a7e534819027bfc4a4856e37eabb7
Marco000000000 <102173222+Marco000000000@users.noreply.github.com>
2024-01-13 13:08:37 +0100

    modifiche per l'uso di flask e holepunch

--- pythonModule/tcp_client.py
+++ pythonModule/tcp_client.py
@@ -4,14 +4,16 @@ import logging
 import socket
 import struct
 from threading import Event, Thread
+import time
 from util import *
+import requests
 
 socket.SO_REUSEPORT = socket.SO_REUSEADDR
 
 logger = logging.getLogger('client')
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 STOP = Event()
-
+STOP.clear()
 
 def accept(port):
     logger.info("accept %s", port)
@@ -24,13 +26,24 @@ def accept(port):
     while not STOP.is_set():
         try:
             conn, addr = s.accept()
+            logger.info("Accept %s connected!", addr)
+            makeThing(conn,"accept")
         except socket.timeout:
             continue
-        else:
-            logger.info("Accept %s connected!", port)
-            # STOP.set()
 
 
+def makeThing(s,type):
+    if type=="connect":
+        while True:
+
+            s.send("salve".encode("utf-8"))
+            logger.info("inviato: %s","salve")
+
+            time.sleep(5)
+    else:
+        while True:
+                msg=s.recv(1000)
+                logger.info("ricevuto: %s",msg.decode("utf-8"))
 def connect(local_addr, addr):
     logger.info("connect from %s to %s", local_addr, addr)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -40,58 +53,65 @@ def connect(local_addr, addr):
     while not STOP.is_set():
         try:
             s.connect(addr)
+            logger.info("connected from %s to %s success!", local_addr, addr)
+
+            makeThing(s,"connect")
+            STOP.set()
         except socket.error:
             continue
         # except Exception as exc:
         #     logger.exception("unexpected exception encountered")
         #     break
-        else:
-            logger.info("connected from %s to %s success!", local_addr, addr)
-            # STOP.set()
+    else:
+        logger.info("connected from %s to %s success!", local_addr, addr)
+        
 
 
 def main(host='localhost', port=8080):
     sa = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sa.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_address = (host, port)
+    httpPort="80"
+    print("http://"+host+":"+httpPort+"/holePunch/1")
+    firstCall=requests.get("http://"+host+":"+httpPort+"/holePunch/1")
+    print(firstCall.text)
+    sa.connect((host, port))
+    priv_addr = sa.getsockname()
+    
+    send_msg(sa, addr_to_msg(priv_addr))
+    data = recv_msg(sa)
+    logger.info("client %s %s - received data: %s", priv_addr[0], priv_addr[1], data)
+    pub_addr = msg_to_addr(data)
+    send_msg(sa, addr_to_msg(pub_addr))
 
-    with socket.create_connection(server_address) as sa:
-        priv_addr = sa.getsockname()
+    data = recv_msg(sa)
+    pubdata, privdata = data.split(b'|')
+    client_pub_addr = msg_to_addr(pubdata)
+    client_priv_addr = msg_to_addr(privdata)
+    logger.info(
+        "client public is %s and private is %s, peer public is %s private is %s",
+        pub_addr, priv_addr, client_pub_addr, client_priv_addr,
+    )
 
-        send_msg(sa, addr_to_msg(priv_addr))
-        data = recv_msg(sa)
-        logger.info("client %s %s - received data: %s", priv_addr[0], priv_addr[1], data)
-        pub_addr = msg_to_addr(data)
-        send_msg(sa, addr_to_msg(pub_addr))
+    threads = {
+        '0_accept': Thread(target=accept, args=(priv_addr[1],)),
+        '1_accept': Thread(target=accept, args=(client_pub_addr[1],)),
+        '2_connect': Thread(target=connect, args=(priv_addr, client_pub_addr,)),
+        '3_connect': Thread(target=connect, args=(priv_addr, client_priv_addr,)),
+    }
+    for name in sorted(threads.keys()):
+        logger.info('start thread %s', name)
+        threads[name].start()
 
-        data = recv_msg(sa)
-        pubdata, privdata = data.split(b'|')
-        client_pub_addr = msg_to_addr(pubdata)
-        client_priv_addr = msg_to_addr(privdata)
-        logger.info(
-            "client public is %s and private is %s, peer public is %s private is %s",
-            pub_addr, priv_addr, client_pub_addr, client_priv_addr,
-        )
+    while threads:
+        keys = list(threads.keys())
+        for name in keys:
+            try:
+                threads[name].join(1)
+            except TimeoutError:
+                continue
+            if not threads[name].is_alive():
+                threads.pop(name)
 
-        threads = {
-            '0_accept': Thread(target=accept, args=(priv_addr[1],)),
-            '1_accept': Thread(target=accept, args=(client_pub_addr[1],)),
-            '2_connect': Thread(target=connect, args=(priv_addr, client_pub_addr,)),
-            '3_connect': Thread(target=connect, args=(priv_addr, client_priv_addr,)),
-        }
-        for name in sorted(threads.keys()):
-            logger.info('start thread %s', name)
-            threads[name].start()
-
-        while threads:
-            keys = list(threads.keys())
-            for name in keys:
-                try:
-                    threads[name].join(1)
-                except TimeoutError:
-                    continue
-                if not threads[name].is_alive():
-                    threads.pop(name)
 
 
 if __name__ == '__main__':
--- pythonModule/tcp_server.py
+++ pythonModule/tcp_server.py
@@ -2,15 +2,18 @@
 import sys
 import logging
 import socket
+from threading import Thread
 from util import *
 socket.SO_REUSEPORT = socket.SO_REUSEADDR
 
+from flask import Flask, Response,request
 
 logger = logging.getLogger()
 clients = {}
+app = Flask(__name__)
+addressConnected=[]
 
-
-def main(host='127.0.0.1', port=5005):
+def main(host='localhost', port=8080):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((host, port))
@@ -20,6 +23,11 @@ def main(host='127.0.0.1', port=5005):
     while True:
         try:
             conn, addr = s.accept()
+            print("indirizzo addr[0]"+addr[0])
+            if addr[0] in addressConnected:
+                addressConnected.remove(addr[0])
+            else:
+                continue
         except socket.timeout:
             continue
 
@@ -47,9 +55,19 @@ def main(host='127.0.0.1', port=5005):
             clients.pop(addr1)
             clients.pop(addr2)
 
-    conn.close()
 
-
+@app.route("/holePunch/<path:userInfo>",methods=['GET'])
+def holePunchControl(userInfo):
+    if userInfo!= "1":
+        return "info errate"
+    else:
+        addressConnected.append(request.remote_addr)
+        return "ok"
+host=""
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
-    main(*addr_from_args(sys.argv))
+    holeThread=Thread(target=main,args=(*addr_from_args(sys.argv),))
+    holeThread.start()
+    #hostname = socket.gethostname()
+    #host=socket.gethostbyname_ex(hostname)[2][0]
+    app.run(debug=True, use_reloader=False,port=80)
