a70f9dd7d06cdd9200e015ff3f0115abf0da6aeb
Marco000000000 <102173222+Marco000000000@users.noreply.github.com>
2024-02-05 11:11:00 +0100

    .

--- pythonModule/client_ipv6.py
+++ pythonModule/client_ipv6.py
@@ -26,7 +26,7 @@ def start_server():
 
 def start_client():
     client = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-    server_address = ("fe80::32b5:c2ff:fea2:a79d%11", 9999)  # Replace with the server's IPv6 address
+    server_address = ("[fe80::d2c7:ac26:1dcf:2fff%11]", 9999)  # Replace with the server's IPv6 address
     
     client.connect(server_address)
 
